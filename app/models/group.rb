c9a56f30c571b67242747b8ee534d3b2fcf564a9
siina-create <ryotagfr5@gmail.com>
2019-11-13 11:45:30 +0900

    グループ作成編集機能
    グループ作成new、編集edit機能をform _forを使った書き方に変更
    グループ作成と編集画面のhamlは _formファイルからrenderで呼び出すように変更
    
    root toをgroup#indexに変更
    hamlはmessageファイルからrenderで呼び出す形にした
    
    グループ編集作成画面の実装に伴い、messageファイルのHAMLに編集、作成画面へのリンクを追加

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,5 @@
 class Group < ApplicationRecord
   has_many :messages
-  has_many :users, through: :users_groups
-  has_many :users_groups
+  has_many :users, through: :user_groups
+  has_many :user_groups
 end
