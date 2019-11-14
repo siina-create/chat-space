93de1323409a7d1aa8922fba9957621916b0f89d
siina-create <ryotagfr5@gmail.com>
2019-11-14 11:54:47 +0900

    edit-side-last-message
    サイドバーにグループごとに最後のメッセージを表示する機能の追加

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,11 @@ class Group < ApplicationRecord
   has_many :messages
   has_many :users, through: :user_groups
   has_many :user_groups
+  def show_last_message
+    if (last_message = messages.last).present?
+      last_message.body? ? last_message.body : '画像が投稿されています'
+    else
+      'まだメッセージはありません。'
+    end
+  end
 end
